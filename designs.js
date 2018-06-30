4f1e7feaf5d2001a01d8df72ed7f57e21d8665c2
Pretty497 <37268461+Pretty497@users.noreply.github.com>
2018-06-30 23:46:44 +0100

    Finalproject on my Challenge
    This is the concluding project of the Udacity Challenge.

---
+++ designs.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+    $('#sizePicker').submit(function makeGrid(grid) {
+    	$('table tr').remove();
+        var rows=$('#inputHeight').val();
+        var cols=$('#inputWeight').val();
+            for(var i=1; i<=rows; i++) {
+            $('table').append('<tr></tr>');
+            for (var j=1; j <= cols; j++){
+            $('tr:last').append('<td></td>');
+            $('td').attr("class", 'cells');
+        }
+    }
+      grid.preventDefault();
+
+$('.cells').click(function(event) {
+	var paint=$('#colorPicker').val();
+	$(event.target).css('background-color',paint);
+}
+	)
+      });
+        });
+    
+    
+  var name = prompt('What is your name?');
+  alert(name +  ',I\'d Love you to see my Pixel Art maker');
+
+  
+  	
